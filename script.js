3cc0b90c88957f83838dee722207c204d2d4a6fa
sshobayo <157627962+sshobayo@users.noreply.github.com>
2024-04-23 00:32:58 +0000

    Project 2 completed

--- script.js
+++ script.js
@@ -15,12 +15,61 @@
 function setup(){
   // this function will run once
   createCanvas(600, 400); // create a 600 x 400 pixel drawing canvas
+  for(var i = 0; i < objects.length; i++) {
+    objects[i] = new sprites(random(width), random(height));
 
 
 }
 
 function draw(){
-  background(200); //light gray background
-  
+  background(255); //light gray background
+  // Make through an array of objects
+  for(va)
+  objects[i].move(); // move the object
+  objects[i].display(); // display the object
 }
+
+this.x = tempX; // X position
+this.y = tempY; // Y position
+this.d = 50; // Diameter
+this.speed = random(-1, 1); // Speed
+
+// Define methods
+this.move = function() {
+  // Move the object
+  this.x += this.speed; // Update X position
+  // Reverse direction if hitting canvas edges
+  if (this.x > width || this.x < 0){
+    this.speed = this.speed * -1; // Reverse direction
+  }
+}
+
+this.display = function(){ 
+  // Display the object
+  push(); // Save current drawing 
+  translate(this.x, this.y); // Move origin to object position
+  fill(255, 0, 0); // Set fill color to red
+  ellipse(0, 0, this.d, this.d); // Draw a circle
+  pop(); // Restore previous drawing
+}
+}
+
+// Create global variables for test instances of your object
+var obj1, obj2;
+
+// Setup function to initialize the canvas and objects
+function setup(){
+createCanvas(320, 240);
+// Create instances of Sprites
+obj1 = new Sprites(100, 100);
+obj2 = new Sprites(200, 100);
+}
+
+// Draw function to animate the objects
+function draw(){
+background(200); // Clear the background
+obj1.move(); // Move object 1
+obj1.display(); // Display object 1
+obj2.move(); // Move object 2
+obj2.display(); // Display object 2
 
